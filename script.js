020c825d43e718c7f1aae319b768315116da5066
S10227809@connect.np.edu.sg <s10227809@connect.np.edu.sg>
2022-02-14 15:24:25 +0800

    get & call API

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let weather = {
+    "apiKey": "3451f3fa3c5b68c76fb330939f273eda",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+        
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900?" + name + "')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
